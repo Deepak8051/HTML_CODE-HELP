47d11140058792708770e292e929a920272063e2
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-14 23:31:56 +0530

    Add files via upload

---
+++ Pre.js
@@ -0,0 +1,50 @@
+let a = 5;
+a++;
+console.log(a)
+let age = 70;
+let status = (age >= 18) ? 'yes vote' : 'no vote';
+console.log(status)
+
+let marks = 98;
+if (marks >= 90) {
+    console.log("mirrst grade A");
+
+}
+else if (marks >= 80) {
+    console.log("B");
+
+}
+else if (marks >= 75) {
+    console.log("C");
+}
+else {
+    console.log("D");
+}
+let no = 2;
+switch (no) {
+    case 1:
+        console.log("Sunday")
+
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("tues");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+
+
+    default:
+      console.log("sunday");
+        break;
+}
+               // loops
+               
